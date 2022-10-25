e48766909437ffac6b08794f57696412e764cf42
Mathis Cadio <cadio@et.esiea.fr>
2022-10-25 13:55:12 +0200

    Update VoitureAPITest.java

--- esieaBack/src/test/java/esiea/api/VoitureAPITest.java
+++ esieaBack/src/test/java/esiea/api/VoitureAPITest.java
@@ -8,27 +8,27 @@ import org.junit.Test;
 import esiea.metier.Voiture;
 
 public class VoitureAPITest {
-  Voiture voiture = new Voiture();
 
   @Test
   public void voitureFromJsonTest() {
     JSONObject json = new JSONObject();
     json.put("id", "0");
-    json.put("marque", "0");
-    json.put("modele", "0");
-    json.put("finition", "0");
-    json.put("carburant", "0");
-    json.put("km", "0");
-    json.put("annee", "0");
-    json.put("prix", "0");
-
-    assertEquals(this.voiture.getId(), 0);
-    assertEquals(this.voiture.getMarque(), 0);
-    assertEquals(this.voiture.getModele(), 0);
-    assertEquals(this.voiture.getFinition(), 0);
-    assertEquals(this.voiture.getCarburant(), 0);
-    assertEquals(this.voiture.getKm(), 0);
-    assertEquals(this.voiture.getAnnee(), 0);
-    assertEquals(this.voiture.getPrix(), 0);
+    json.put("marque", "Renault");
+    json.put("modele", "Twingo");
+    json.put("finition", "Dynamique");
+    json.put("carburant", "E");
+    json.put("km", "11111");
+    json.put("annee", "2010");
+    json.put("prix", "4000");
+    VoitureAPI voitureApi = new VoitureAPI();
+    Voiture voiture = voitureApi.voitureFromJson(json);
+    assertEquals(voiture.getId(), 0);
+    assertEquals(voiture.getMarque(), "Renault");
+    assertEquals(voiture.getModele(), "Twingo");
+    assertEquals(voiture.getFinition(), "Dynamique");
+    assertEquals(voiture.getCarburant().toString(), "E");
+    assertEquals(voiture.getKm(), 11111);
+    assertEquals(voiture.getAnnee(), 2010);
+    assertEquals(voiture.getPrix(), 4000);
   }
 }
